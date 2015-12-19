3a951a7a1a64a5b6894e0d5ab1569866818a1188
jwl <james.warren.leung@gmail.com>
2015-12-18 21:12:45 -0700

    Implemented flask versions of functions for creating new menu items and editing existing menu items.

--- vagrant/ud088-restaurants/routingprojecttemplate.py
+++ vagrant/ud088-restaurants/routingprojecttemplate.py
@@ -30,7 +30,7 @@ def newMenuItem(restaurant_id):
         session.commit()
         return redirect(url_for('restaurantMenu', \
                 restaurant_id = restaurant_id))
-    else:
+    else: # GET request
         return render_template('newmenuitem.html', \
                 restaurant_id = restaurant_id)
 
@@ -38,9 +38,27 @@ def newMenuItem(restaurant_id):
 # Task 2: Create route for editMenuItem function here
 
 
-@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/', 
+        methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    return "page to edit a menu item. Task 2 complete!"
+    editedItem = session.query(MenuItem).filter_by(id = menu_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', \
+                restaurant_id = restaurant_id))
+    else: # GET request
+        return render_template('editmenuitem.html', \
+                restaurant_id = restaurant_id, \
+                menu_id = menu_id, \
+                item = editedItem)
+
+
+
+
+
 
 # Task 3: Create a route for deleteMenuItem function here
 
