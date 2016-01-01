4e8edb9f343b87840d87f69c64fa2b3732cb0328
jwl <james.warren.leung@gmail.com>
2015-12-31 23:37:19 -0700

    Implemented stub methods for edit/delete for puppies, shelters and adopters. Implemented list pages and profile pages for adopters and shelters.

--- vagrant/ud088-ps02_puppy_flask_db/project.py
+++ vagrant/ud088-ps02_puppy_flask_db/project.py
@@ -23,7 +23,7 @@ def puppy_list():
 
 @app.route('/puppies/<int:puppy_id>')
 def puppyProfile(puppy_id):
-    print "puppyProfile, puppy_id: ", puppy_id
+    # print "puppyProfile, puppy_id: ", puppy_id
     puppy = session.query(Puppy).filter_by(puppy_id = puppy_id).one()
     return render_template(
             'puppy_profile.html', puppy = puppy)
@@ -42,6 +42,19 @@ def newPuppy(puppy_id):
         return render_template('new_puppy.html', \
                 puppy_id = puppy_id)
 
+@app.route('/puppies/<int:puppy_id>/edit/')
+def editPuppy(puppy_id):
+    # TODO: implement edit functionality
+    shelter = session.query(Puppy).filter_by(puppy_id = puppy_id).one()
+    return render_template(
+            'puppy_profile.html', puppy = puppy)
+
+@app.route('/puppy/<int:puppy_id>/delete/')
+def deletePuppy(puppy_id):
+    # TODO: implement delete functionality
+    shelter = session.query(Puppy).filter_by(puppy_id = puppy_id).one()
+    return render_template(
+            'puppy_profile.html', puppy = puppy)
 
 
 # CRUD functionality for shelters
@@ -51,6 +64,26 @@ def shelter_list():
     return render_template(
             'shelter_list.html', items = shelters)
 
+@app.route('/shelters/<int:shelter_id>')
+def shelterProfile(shelter_id):
+    shelter = session.query(Shelter).filter_by(shelter_id = shelter_id).one()
+    return render_template(
+            'shelter_profile.html', shelter = shelter)
+
+@app.route('/shelters/<int:shelter_id>/edit/')
+def editShelter(shelter_id):
+    # TODO: implement edit functionality
+    shelter = session.query(Shelter).filter_by(shelter_id = shelter_id).one()
+    return render_template(
+            'shelter_profile.html', shelter = shelter)
+
+@app.route('/shelters/<int:shelter_id>/delete/')
+def deleteShelter(shelter_id):
+    # TODO: implement delete functionality
+    shelter = session.query(Shelter).filter_by(shelter_id = shelter_id).one()
+    return render_template(
+            'shelter_profile.html', shelter = shelter)
+
 
 
 # CRUD functionality for adopters
@@ -59,6 +92,33 @@ def adopter_list():
     adopters = session.query(Adopter).order_by(Adopter.adopter_id)
     return render_template(
             'adopter_list.html', items = adopters)
+
+@app.route('/adopters/<int:adopter_id>')
+def adopterProfile(adopter_id):
+    adopter = session.query(Adopter).filter_by(adopter_id = adopter_id).one()
+    return render_template(
+            'adopter_profile.html', adopter = adopter)
+
+@app.route('/adopters/<int:adopter_id>/edit/')
+def editAdopter(adopter_id):
+    # TODO: implement edit functionality
+    adopter = session.query(Adopter).filter_by(adopter_id = adopter_id).one()
+    return render_template(
+            'adopter_profile.html', adopter = adopter)
+
+@app.route('/adopters/<int:adopter_id>/delete/')
+def deleteAdopter(adopter_id):
+    # TODO: implement delete functionality
+    adopter = session.query(Adopter).filter_by(adopter_id = adopter_id).one()
+    return render_template(
+            'adopter_profile.html', adopter = adopter)
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
